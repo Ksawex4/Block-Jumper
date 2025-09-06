e818b0f3e9c65c734792c05c0e85e12faaf2f1e0
Ksawex <ksawex17@gmail.com>
2025-09-06 15:17:47 +0200

    Added a new Spamguy "SpamQueen"

--- Scripts/Characters/Enemies/spamguy.gd
+++ Scripts/Characters/Enemies/spamguy.gd
@@ -5,24 +5,29 @@ var direction := 1
 var wallCooldown := 0.0
 
 func _ready() -> void:
-	var rand = randi_range(0,3)
+	var rand = randi_range(0,4)
 	AchievMan.AddAchievement("Trash")
-	if rand == 0:
-		$BDSpamguy.disabled = false
-		$Sprite2D.texture = load("res://Assets/Sprites/Characters/Enemies/BadlyDrawnSpamton.png")
-		speed = 4200
-	if rand == 1:
-		$Brunton1.disabled = false
-		$Sprite2D.texture = load("res://Assets/Sprites/Characters/Enemies/Brunton1.png")
-		speed = 7800
-	if rand == 2:
-		$Brunton2.disabled = false
-		$Sprite2D.texture = load("res://Assets/Sprites/Characters/Enemies/Brunton2.png")
-		speed = 7800
-	if rand == 3:
-		$Ksawton.disabled = false
-		$Sprite2D.texture = load("res://Assets/Sprites/Characters/Enemies/Ksawton.png")
-		speed = 6000
+	match rand:
+		0:
+			$BDSpamguy.disabled = false
+			$Sprite2D.texture = load("res://Assets/Sprites/Characters/Enemies/BadlyDrawnSpamton.png")
+			speed = 4200
+		1:
+			$Brunton1.disabled = false
+			$Sprite2D.texture = load("res://Assets/Sprites/Characters/Enemies/Brunton1.png")
+			speed = 7800
+		2:
+			$Brunton2.disabled = false
+			$Sprite2D.texture = load("res://Assets/Sprites/Characters/Enemies/Brunton2.png")
+			speed = 7800
+		3:
+			$Ksawton.disabled = false
+			$Sprite2D.texture = load("res://Assets/Sprites/Characters/Enemies/Ksawton.png")
+			speed = 6000
+		4:
+			$BDSpamQueen.disabled = false
+			$Sprite2D.texture = load("res://Assets/Sprites/Characters/Enemies/BadlyDrawnSpamQueen.png")
+			speed = 7000
 
 func _physics_process(delta: float) -> void:
 	if !is_on_floor():
