09c01b171775e914b701faf0626db40564533940
Ksawex <ksawex17@gmail.com>
2025-11-06 17:40:44 +0100

    Fixed the stick not loading its type (fixes #4)

--- Scripts/Objects/Items/stick.gd
+++ Scripts/Objects/Items/stick.gd
@@ -1,50 +1,59 @@
 extends CharacterBody2D
 
-@export var who: String = ""
-@export var noPlayer := false
-var stickType = "Normal"
-var player: Node2D
-var damage := 40
+@export var Who: String = ""
+@export var No_player := false
+var Stick_type := "Normal"
+var Player: Node2D
+var Damage := 40
 
 func _ready() -> void:
-	player = NovaFunc.GetPlayerFromGroup(who)
-	if noPlayer:
+	Player = NovaFunc.GetPlayerFromGroup(Who)
+	if No_player:
 		set_collision_mask_value(1, true)
+	elif not No_player and Who != "":
+		Stick_type = PlayerStats.AllPlayerStats[Who]["StickType"]
 	
-	match stickType:
+	match Stick_type:
 		"Normal": pass
 		"Cheese":
-			damage = 20
-			$Sprite2D.texture = load("res://Assets/Sprites/Objects/Items/CheeseStick.png")
+			Damage = 20
+			$Sprite2D.texture = preload("uid://bln2uwbsqudqs")
+
 
 func _process(_delta: float) -> void:
-	if !noPlayer and who != "":
-		if player != null:
-			position = player.position
+	if not No_player and Who != "":
+		if Player != null:
+			position = Player.position
 		else:
 			queue_free()
-		if PlayerStats.AllPlayerStats[who]["Stick"] == false:
+		if PlayerStats.AllPlayerStats[Who]["Stick"] == false:
 			queue_free()
 
+
 func _physics_process(delta: float) -> void:
-	if noPlayer:
-		if !is_on_floor():
+	if No_player:
+		if not is_on_floor():
 			velocity.y += LevelMan.Gravity * delta
 		move_and_slide()
 
+
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if not noPlayer and who != "":
-		if body.has_method("takeDamage") and not body.name.begins_with("Boss") or body.name.begins_with("Boss") and LevelMan.BossFightOn:
-			body.takeDamage(damage)
-			PlayerStats.AllPlayerStats[who]["Stick"] = false
+	if not No_player and Who != "":
+		if (
+		body.has_method("takeDamage") and not body.name.begins_with("Boss") 
+		or body.name.begins_with("Boss") and LevelMan.BossFightOn
+		):
+			body.takeDamage(Damage)
+			PlayerStats.AllPlayerStats[Who]["Stick"] = false
 			queue_free()
 
+
 func _on_pick_up_area_body_entered(body: Node2D) -> void:
-	if !PlayerStats.AllPlayerStats[body.name]["Stick"] and who == "":
+	if not PlayerStats.AllPlayerStats[body.name]["Stick"] and Who == "":
 		AchievMan.AddAchievement("Stick")
-		who = body.whoAmI
-		player = body
+		Who = body.whoAmI
+		Player = body
 		set_collision_mask_value(1, false)
 		PlayerStats.AllPlayerStats[body.name]["Stick"] = true
-		PlayerStats.AllPlayerStats[body.name]["StickType"] = stickType
-		noPlayer = false
+		PlayerStats.AllPlayerStats[body.name]["StickType"] = Stick_type
+		No_player = false
